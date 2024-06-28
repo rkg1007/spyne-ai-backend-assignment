3d60844edc8aa7e6d363bc6b17a4e7bb08724930
rohit <rkg10072000@gmail.com>
2024-06-28 15:40:47 +0530

    add mysql database

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtProvider } from './providers/jwt.provider';
 import { HashingProvider } from './providers/hashing.provider';
+import { createPool } from 'mysql2';
+import { DatabaseProvider } from './providers/database.provider';
 
 @Global()
 @Module({
@@ -19,7 +21,25 @@ import { HashingProvider } from './providers/hashing.provider';
       inject: [ConfigService],
     }),
   ],
-  providers: [JwtProvider, HashingProvider],
-  exports: [JwtProvider, HashingProvider],
+  providers: [
+    JwtProvider,
+    HashingProvider,
+    {
+      provide: 'DATABASE_POOL',
+      useFactory: async (configService: ConfigService) => {
+        const pool = createPool({
+          host: configService.get<string>('DB_HOST'),
+          port: configService.get<number>('DB_PORT'),
+          user: configService.get<string>('DB_USER'),
+          password: configService.get<string>('DB_PASSWORD'),
+          database: configService.get<string>('DB_NAME'),
+        });
+        return pool;
+      },
+      inject: [ConfigService],
+    },
+    DatabaseProvider,
+  ],
+  exports: [JwtProvider, HashingProvider, DatabaseProvider],
 })
 export class CommonModule {}
